8a0a9a75015581f97bada660862b644fa8be06ab
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-05-23 00:31:30 +0300

    Update message page

--- components/message/details.js
+++ components/message/details.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {Animated, Keyboard, AppRegistry, Text, Image, View, StyleSheet, TextInput, TouchableOpacity, ScrollView} from 'react-native';
-import socketIO from 'socket.io-client'
-import { API_KEY } from 'react-native-dotenv'
+import socketIO from 'socket.io-client';
+import Constants from 'expo-constants';
+import { API_KEY } from 'react-native-dotenv';
 
 import Header from '../partials/header';
 
@@ -37,29 +38,6 @@ export default class MessageDetails extends Component{
     this.messagesWrapperMarginBottom = new Animated.Value(0);
   };
 
-  componentWillMount () {
-    fetch('https://stumarkt.herokuapp.com/api/messages?buyer=' + this.state.buyer._id + '&product=' + this.state.product._id + '&sendedBy=' + (this.state.messagesOf == 'buyer' ? 'owner' : 'buyer') + '&user=' + this.state.user._id, {
-      headers: {
-        "x_auth": API_KEY
-      }
-    })
-      .then(response => {return response.json()})
-      .then(data => {
-        if (data.error) return alert("Err: " + data.error);
-
-        this.setState({
-          "messages": data.message.messages,
-          "message": data.message
-        });
-      })
-      .catch((err) => {
-        return alert("Err: " + err);
-      });
-
-    this.keyboardWillShowSub = Keyboard.addListener('keyboardDidShow', this.keyboardWillShow);
-    this.keyboardWillHideSub = Keyboard.addListener('keyboardDidHide', this.keyboardWillHide);
-  };
-
   componentWillUnmount() {
     this.keyboardWillShowSub.remove();
     this.keyboardWillHideSub.remove();
@@ -70,33 +48,41 @@ export default class MessageDetails extends Component{
 
     Animated.parallel([
       Animated.timing(this.keyboardHeight, {
-        duration: 0,
-        toValue: event.endCoordinates.height + 10,
+        duration: (Platform.OS == "ios" ? event.duration : 0),
+        toValue: event.endCoordinates.height + (Platform.OS == "ios" ? 10 : 10),
       }),
       Animated.timing(this.messagesWrapperMarginBottom, {
-        duration: 0,
+        duration: (Platform.OS == "ios" ? event.duration : 0),
         toValue: event.endCoordinates.height + this.state.messageInputWrapperHeight,
       })
     ]).start();
     setTimeout(() => {
-      this.scrollView.current._component.scrollToEnd({animated: true});
-    }, 10);
+      this.scrollView.current._component.scrollToEnd({animated: Platform.OS == "ios"});
+    }, (Platform.OS == "ios" ? event.duration + 100 : 100));
   };
 
   keyboardWillHide = (event) => {
     Animated.parallel([
       Animated.timing(this.keyboardHeight, {
-        duration: 0,
+        duration: (Platform.OS == "ios" ? event.duration : 0),
         toValue: 20,
       }),
       Animated.timing(this.messagesWrapperMarginBottom, {
-        duration: 0,
+        duration: (Platform.OS == "ios" ? event.duration : 0),
         toValue: this.state.messageInputWrapperHeight
       })
     ]).start();
   };
 
-  componentDidMount() {
+  componentDidMount = () => {
+    if (Platform.OS == "ios") {
+      this.keyboardWillShowSub = Keyboard.addListener('keyboardWillShow', this.keyboardWillShow);
+      this.keyboardWillHideSub = Keyboard.addListener('keyboardWillHide', this.keyboardWillHide);
+    } else {
+      this.keyboardWillShowSub = Keyboard.addListener('keyboardDidShow', this.keyboardWillShow);
+      this.keyboardWillHideSub = Keyboard.addListener('keyboardDidHide', this.keyboardWillHide);
+    }
+
     const socket = socketIO('https://stumarkt.herokuapp.com');
     
     socket.connect();
@@ -122,6 +108,24 @@ export default class MessageDetails extends Component{
     if (this.props.navigation.getParam('messageContent', undefined)) {
       this.refs._messageInput.focus();
     }
+
+    fetch('https://stumarkt.herokuapp.com/api/messages?buyer=' + this.state.buyer._id + '&product=' + this.state.product._id + '&sendedBy=' + (this.state.messagesOf == 'buyer' ? 'owner' : 'buyer') + '&user=' + this.state.user._id, {
+      headers: {
+        "x_auth": API_KEY
+      }
+    })
+      .then(response => {return response.json()})
+      .then(data => {
+        if (data.error) return alert("Err: " + data.error);
+  
+        this.setState({
+          "messages": data.message.messages,
+          "message": data.message
+        });
+      })
+      .catch((err) => {
+        return alert("Err: " + err);
+      });
   };
 
   onNewMessageSend = () => {
@@ -165,6 +169,17 @@ export default class MessageDetails extends Component{
             <TouchableOpacity onPress={() => {this.props.navigation.push("messageDashboard", {"user": this.state.user});}} >
               <Image source={require('./../../assets/back-button.png')} style={styles.navButtonImage} ></Image>
             </TouchableOpacity>
+            { this.state.messagesOf == "buyer" ? 
+              <View style={{flexDirection: "row", alignItems: "center"}} >
+                <Image source={{uri: this.state.owner.profilePhoto}} style={styles.userProfilePhoto} ></Image>
+                <Text style={styles.userName} >{this.state.owner.name}</Text>
+              </View>
+              :
+              <View style={{flexDirection: "row", alignItems: "center"}} >
+                <Image source={{uri: this.state.buyer.profilePhoto}} style={styles.userProfilePhoto} ></Image>
+                <Text style={styles.userName} >{this.state.buyer.name}</Text>
+              </View>
+            }
           </View>
           <Animated.ScrollView style={[styles.messageWrapper, {marginBottom: this.state.clicked ? this.messagesWrapperMarginBottom : this.state.messageInputWrapperHeight}]} ref={this.scrollView}
             onContentSizeChange={() => {  
@@ -173,31 +188,71 @@ export default class MessageDetails extends Component{
             {
               this.state.messages.map((message, key) => {
                 if (message.sendedBy == this.state.messagesOf) {
-                  return (
-                    <View key={key} style={[styles.eachMessage, {marginLeft: "auto"}]} >
-                      <View style={[styles.eachMessageContent, {marginRight: 20, alignItems: "flex-end"}]} >
-                        <View style={[styles.eachMessageHeader, {alignItems: "flex-end"}]} >
-                          <Text style={styles.eachMessageSenderName} >{this.state.message[this.state.messagesOf].name}</Text>
-                          <Text style={styles.eachMessageDate} >{message.createdAt}</Text>
+                  if (key == 0) {
+                    return (
+                      <View key={key} >
+                        <View style={styles.day_wrapper} >
+                          <Text style={styles.each_day} >{message.day}</Text>
                         </View>
-                        <Text style={[styles.eachMessageText, {textAlign: "right"}]} >{message.content}</Text>
+                        <View style={styles.each_message_wrapper_user} >
+                          <Text style={styles.each_message_text_user} >{message.content}</Text>
+                          <Text style={styles.each_message_time_user} >{message.time}</Text>
+                        </View>
                       </View>
-                      <Image style={styles.eachMessageProfile} source={{uri: this.state[message.sendedBy].profilePhoto}} ></Image>
-                    </View>
-                  )
+                    );
+                  } else if (message.day != this.state.messages[key-1].day) {
+                    return (
+                      <View key={key} >
+                        <View style={styles.day_wrapper} >
+                          <Text style={styles.each_day} >{message.day}</Text>
+                        </View>
+                        <View style={styles.each_message_wrapper_user} >
+                          <Text style={styles.each_message_text_user} >{message.content}</Text>
+                          <Text style={styles.each_message_time_user} >{message.time}</Text>
+                        </View>
+                      </View>
+                    );
+                  } else {
+                    return (
+                      <View key={key} style={styles.each_message_wrapper_user} >
+                        <Text style={styles.each_message_text_user} >{message.content}</Text>
+                        <Text style={styles.each_message_time_user} >{message.time}</Text>
+                      </View>
+                    );
+                  }
                 } else {
-                  return (
-                    <View key={key} style={styles.eachMessage} >
-                      <Image style={styles.eachMessageProfile} source={{uri: this.state[message.sendedBy].profilePhoto}} ></Image>
-                      <View style={[styles.eachMessageContent, {marginLeft: 20}]} >
-                        <View style={styles.eachMessageHeader} >
-                          <Text style={styles.eachMessageSenderName} >{this.state.message[message.sendedBy].name}</Text>
-                          <Text style={styles.eachMessageDate} >{message.createdAt}</Text>
+                  if (key == 0) {
+                    return (
+                      <View key={key} >
+                        <View style={styles.day_wrapper} >
+                          <Text style={styles.each_day} >{message.day}</Text>
                         </View>
-                        <Text style={styles.eachMessageText} >{message.content}</Text>
+                        <View style={styles.each_message_wrapper} >
+                          <Text style={styles.each_message_text} >{message.content}</Text>
+                          <Text style={styles.each_message_time} >{message.time}</Text>
+                        </View>
                       </View>
-                    </View>
-                  )
+                    );
+                  } else if (message.day != this.state.messages[key-1].day) {
+                    return (
+                      <View key={key} >
+                        <View style={styles.day_wrapper} >
+                          <Text style={styles.each_day} >{message.day}</Text>
+                        </View>
+                        <View style={styles.each_message_wrapper} >
+                          <Text style={styles.each_message_text} >{message.content}</Text>
+                          <Text style={styles.each_message_time} >{message.time}</Text>
+                        </View>
+                      </View>
+                    );
+                  } else {
+                    return (
+                      <View key={key} style={styles.each_message_wrapper} >
+                        <Text style={styles.each_message_text} >{message.content}</Text>
+                        <Text style={styles.each_message_time} >{message.time}</Text>
+                      </View>
+                    );
+                  }
                 }
               })
             }
@@ -227,38 +282,59 @@ const styles = StyleSheet.create({
     backgroundColor: "rgb(252, 249, 247)",
   },
   contentHeader: {
-    justifyContent: "center",
+    alignItems: "center",
     height: 50, paddingLeft: 20, paddingRight: 20,
-    backgroundColor: "white",
+    backgroundColor: "white", flexDirection: "row",
     borderBottomWidth: 2, borderBottomColor: "rgb(236, 235, 235)",
+  },
+  userProfilePhoto: {
+    width: 30, height: 30,
+    resizeMode: "contain", borderRadius: 15,
+    marginLeft: 20
+  },
+  userName: {
+    fontSize: 15, color: "rgb(112, 112, 112)", fontWeight: "600",
+    marginLeft: 10
   },
   messageWrapper: {
     padding: 20
   },
-  eachMessage: {
-    width: "90%",
-    flexDirection: "row", alignItems: "center",
-    marginBottom: 25,
+  day_wrapper: {
+    alignSelf: "center", marginBottom: 15,
+    backgroundColor: "rgb(236, 236, 236)",
+    padding: 10, paddingLeft: 15, paddingRight: 15,
+    borderRadius: 10
   },
-  eachMessageContent: {
-    flex: 1
+  each_day: {
+    fontSize: 15, fontWeight: "300", color: "rgb(112, 112, 112)"
   },
-  eachMessageHeader: {
-    marginBottom: 5
+  each_message_wrapper_user: {
+    maxWidth: "90%", alignItems: "flex-end",
+    marginBottom: 15, marginLeft: "auto", padding: 15,
+    borderRadius: 25, borderTopRightRadius: 0,
+    backgroundColor: "rgba(255, 61, 148, 0.8)"
   },
-  eachMessageSenderName: {
-    fontSize: 20, color: "rgb(127, 127, 127)", fontWeight: "600",
-    marginBottom: 5
+  each_message_text_user: {
+    color: "rgb(251, 251, 251)", fontWeight: "600", fontSize: 17,
+    lineHeight: 25
   },
-  eachMessageDate: {
-    fontSize: 15, color: "rgb(219, 219, 219)", fontWeight: "600"
+  each_message_time_user: {
+    color: "rgb(251, 251, 251)", fontWeight: "400", fontSize: 15,
+    marginTop: 3
   },
-  eachMessageText: {
-    fontSize: 18, color: "rgb(160, 169, 177)", fontWeight: "300"
+  each_message_wrapper: {
+    maxWidth: "90%", marginRight: "auto", marginBottom: 15, padding: 15,
+    borderRadius: 25, borderTopLeftRadius: 0,
+    backgroundColor: "rgb(251, 251, 251)",
+    borderColor: "rgb(236, 236, 236)", borderWidth: 2
   },
-  eachMessageProfile: {
-    height: 50, width: 50, resizeMode: "cover",
-    borderRadius: 25
+  each_message_text: {
+    color: "rgb(112, 112, 112)", fontWeight: "500", fontSize: 17,
+    lineHeight: 25
+  },
+  each_message_time: {
+    color: "rgba(112, 112, 112, 0.8)", fontWeight: "400", fontSize: 15,
+    marginTop: 3
   },
   messageInputWrapper: {
     position: "absolute", width: "96%", height: "10%",
@@ -270,7 +346,7 @@ const styles = StyleSheet.create({
   messageInput: {
     flex: 5, height: "90%",
     paddingLeft: 15,
-    fontSize: 20, color: "rgb(112, 112, 112)",
+    fontSize: 20, color: "rgb(112, 112, 112)"
   },
   messageSendButton: {
     flex: 1, height: "90%",
